c3ece10bd37a1aae0dbf475d2bdfe6aef5963664
malancas-anamaria-niculina <anamaria_niculina@yahoo.com>
2023-06-10 17:06:36 +0300

    Delete Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,6 @@ import Login from "./Components/Login";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h3>Routes</h3>
-      </header>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/map" element={<Map />} />
