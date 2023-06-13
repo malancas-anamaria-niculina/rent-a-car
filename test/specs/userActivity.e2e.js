4bb66d468171736a505122f3068dcbfe828b6470
Tudor Andrei Iosif <tudor.iosif@saltandpepper.co>
2023-06-13 19:39:49 +0300

    fix tests

--- test/specs/userActivity.e2e.js
+++ test/specs/userActivity.e2e.js
@@ -1,18 +1,9 @@
 describe("user-activity", () => {
   it("tests user-activity", async () => {
-    await browser.setNetworkConditions({
-      offline: false,
-      latency: 562.5,
-      download_throughput: 180000,
-      upload_throughput: 84375,
-    });
-    await browser.setWindowSize(1000, 937);
     await browser.url("http://localhost:3000/");
     await expect(browser).toHaveUrl("http://localhost:3000/");
-    await browser.$("#email").click();
-    await browser.$("#email").setValue("user@example.com");
-    await browser.$("#password").click();
-    await browser.$("#password").setValue("Password11@");
+    await browser.$("#email").setValue("test3@test.com");
+    await browser.$("#password").setValue("TestUser123@");
     await browser.$("aria/Log in").click();
     await expect(browser).toHaveUrl("http://localhost:3000/dashboard");
     await browser.$("aria/User Activity").click();
